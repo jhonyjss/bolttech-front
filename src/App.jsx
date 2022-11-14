8342ef58792d7165bed237c7fc4abde3dee921e3
Jhony Souza <jhony.jss@gmail.com>
2022-11-13 21:19:54 -0300

    chore: basic protected page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { Routes, Route } from "react-router-dom";
+import { ProtectedRoute } from "./routes";
+
 import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route
+        path="/register"
+        element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
---
+++ src/pages/register.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import BTFormRegister from "../components/auth/BTFormRegister";
+const Login = () => {
+    return <BTFormRegister />;
+};
+
+export default Login;
