a6f731f4d2ca12c4a76268e67b78329307848783
Jhony Souza <jhony.jss@gmail.com>
2022-11-16 11:52:33 -0300

    chore: files fixed to the branch

--- src/pages/Login.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import BTFormLogin from "../components/auth/BTFormLogin";
-type Props = {};
-const Login: React.FC = ({}: Props) => {
-  return <BTFormLogin />;
-};
-
-export default Login;
